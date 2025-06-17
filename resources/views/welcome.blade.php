43768a8819bc05969f18ff68eb5eca73a02774ff
wvallone <william.vallone@ags-it.com>
2025-06-17 18:44:38 +0200

    logo

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -77,7 +77,7 @@
 
             @endif
         </header>
-        <div class="flex items-center justify-center w-full transition-opacity opacity-100 duration-750 lg:grow starting:opacity-0">
+        <div class="flex items-center justify-center w-full transition-opacity opacity-100 duration-750 lg:grow starting:opacity-0" style="background-image: url('/images/sfondo.PNG')">
            
             <!-- <main class="flex max-w-[335px] w-full flex-col-reverse lg:max-w-4xl lg:flex-row lg:h-auto"> -->
             <main class="flex max-w-[335px] w-full flex-col-reverse lg:max-w-4xl lg:flex-row lg:h-auto items-start justify-start">
