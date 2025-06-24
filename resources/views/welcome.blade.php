b50e4d1644096020baa787be5e19ccaa80a18912
wvallone <william.vallone@ags-it.com>
2025-06-24 12:52:02 +0200

    new

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -19,7 +19,7 @@
 
     <header class="w-full lg:max-w-4xl max-w-[335px] text-sm mb-6 not-has-[nav]:hidden">
             @if (Route::has('login'))
-                <!-- <nav class="flex items-center justify-end gap-4">
+                <!-- <nav class="flex items-center justify-end gap-
                     @auth
                         
                     @else
