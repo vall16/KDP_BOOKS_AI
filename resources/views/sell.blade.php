fa4725e71d0c7dba2a2dce441ae07c174b94f44c
wvallone <william.vallone@ags-it.com>
2025-06-25 17:37:54 +0200

    varie

--- resources/views/sell.blade.php
+++ resources/views/sell.blade.php
@@ -13,14 +13,14 @@
         <div class="grid grid-cols-1 md:grid-cols-3 gap-8">
           @foreach(config('pacchetti') as $codice => $pacchetto)
           <div @click="selected = '{{ $codice }}'"
-              :class="selected === '{{ $codice }}' ? 'ring ring-orange-300' : ''"
-              class="package bg-black text-white border-2 border-orange-500 shadow-md rounded-lg p-6 text-center cursor-pointer transition">
-            <h3 class="text-3xl font-semibold mb-4 border-b border-orange-500 pb-2">{{ $pacchetto['name'] }}</h3>
+              :class="selected === '{{ $codice }}' ? 'ring ring-purple-300' : ''"
+              class="package bg-black text-white border-2 border-purple-500 shadow-md rounded-lg p-6 text-center cursor-pointer transition">
+            <h3 class="text-3xl font-semibold mb-4 border-b border-purple-500 pb-2">{{ $pacchetto['name'] }}</h3>
             
             <div class="h-6"></div>
             @foreach ($pacchetto['description_lines'] as $line)
             <li class="flex items-start gap-2 text-gray-300">
-                <span class="text-orange-500">&gt;</span>
+                <span class="text-purple-500">&gt;</span>
                 <span class="text-lg">{{ $line }}</span>
             </li>
              @endforeach 
@@ -29,10 +29,10 @@
 
             
 
-            <p class="text-2xl font-bold mb-4 border-b border-orange-500 pb-2">€{{ number_format($pacchetto['price'], 2, ',', '.') }}</p>
+            <p class="text-2xl font-bold mb-4 border-b border-purple-500 pb-2">€{{ number_format($pacchetto['price'], 2, ',', '.') }}</p>
 
           <!-- <button
-              class="create-btn inline-block px-8 py-3 bg-black text-white font-bold text-lg border-4 border-orange-500 rounded-full hover:bg-orange-500 transition loader-link"
+              class="create-btn inline-block px-8 py-3 bg-black text-white font-bold text-lg border-4 border-purple-500 rounded-full hover:bg-purple-500 transition loader-link"
               @click.stop="
                 selected = '{{ $codice }}';
                 window.location.href = '/crea-libro?pack={{ $codice }}'
@@ -41,7 +41,7 @@
           </button> -->
 
           <button
-            class="create-btn inline-block px-8 py-3 bg-black text-white font-bold text-lg border-4 border-orange-500 rounded-full hover:bg-orange-500 transition loader-link"
+            class="create-btn inline-block px-8 py-3 bg-black text-white font-bold text-lg border-4 border-purple-500 rounded-full hover:bg-purple-500 transition loader-link"
             @click.stop="
               selected = '{{ $codice }}';
               window.location.href = '{{ Auth::check() ? url('/crea-libro?pack=' . $codice) : route('auth.google') }}'
