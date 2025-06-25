c55e6284752df8214b4324adeeacda34c192188e
wvallone <william.vallone@ags-it.com>
2025-06-25 17:06:07 +0200

    terms

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -10,6 +10,10 @@
 
         <title>{{ config('app.name', 'xxx') }}</title>
 
+        <!-- ✅ Favicon -->
+        <link rel="icon" href="{{ asset('images/easybookai_logo.png') }}" type="image/png">
+
+
         <!-- Fonts -->
         <link rel="preconnect" href="https://fonts.bunny.net">
         <link href="https://fonts.bunny.net/css?family=figtree:400,500,600&display=swap" rel="stylesheet" />
